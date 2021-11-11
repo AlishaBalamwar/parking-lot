efb3e1a0f458afa42d55f6b5468ef6581623bfda
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-11 14:59:40 +0530

    Added: Testcase for given a vehicle should be unparked

--- src/main/java/com/bridgelabz/parkinglot/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglot/ParkingLotSystem.java
@@ -4,7 +4,21 @@ public class ParkingLotSystem {
 
     private Object vehicle;
 
+    public ParkingLotSystem() {
+    }
+
     public boolean park(Object vehicle) {
+        if (this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean UnPark(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/bridgelabz/parkinglot/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/parkinglot/ParkingLotSystemTest.java
@@ -1,14 +1,38 @@
 package com.bridgelabz.parkinglot;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 public class ParkingLotSystemTest {
 
+    Object vehicle = null;
+    ParkingLotSystem parkingLot = null;
+
+    @Before
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingLot = new ParkingLotSystem();
+    }
+
     @Test
-    public void givenAVehicle_whenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLot = new ParkingLotSystem();
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
         boolean isPark = parkingLot.park(new Object());
         Assert.assertTrue(isPark);
+    }
+
+    @Test
+    public void givenAVehicle_WhenAlreadyPark_ShouldReturnFalse() {
+        parkingLot.park(vehicle);
+        boolean isPark = parkingLot.park(new Object());
+        Assert.assertFalse(isPark);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLot.park(vehicle);
+        boolean isUnParked = parkingLot.UnPark(vehicle);
+        Assert.assertTrue(isUnParked);
     }
 }
